2daf7982b8c9841896636ba9f063acf3b1f08bc7
LinCie <aldiofernanda@protonmail.com>
2025-04-17 17:04:35 +0700

    feat: add refresh token route

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Elysia, t } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 import { JWTSECRET } from "@/config";
 import { AuthService } from "./auth.service";
+import { UsersService } from "../users/users.service";
 
 const authModule = new Elysia({ prefix: "/api/auth" })
 	.use(
 		jwt({
 			name: "jwt",
 			secret: JWTSECRET,
+			exp: "30d",
 		})
 	)
 	.decorate("authService", new AuthService())
+	.decorate("usersService", new UsersService())
 	.post(
 		"/register",
 		async ({ authService, body, jwt, set }) => {
@@ -32,6 +35,23 @@ const authModule = new Elysia({ prefix: "/api/auth" })
 		{
 			body: t.Object({ username: t.String(), password: t.String() }),
 			detail: { tags: ["Auth"] },
+		}
+	)
+	.get(
+		"/refresh",
+		async ({ jwt, headers: { authorization }, usersService, error }) => {
+			const bearer = authorization?.split(" ")[1];
+			const token = await jwt.verify(bearer);
+			if (!token) {
+				return;
+			}
+
+			const user = await usersService.getUserById(Number(token.sub));
+			if (!user) {
+				return error(404);
+			}
+
+			return jwt.sign({ sub: String(user.id) });
 		}
 	);
 
